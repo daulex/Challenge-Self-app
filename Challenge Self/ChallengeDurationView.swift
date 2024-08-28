009c1dd2ffef5d6d16b4e47b39ca3db4ba56923c
Kirills Galenko <kirillgalenko@gmail.com>
2024-08-28 16:49:56 +0300

    Added ability to proceed to the next view once challenge items are selected, fixed issue with double back

---
+++ Challenge Self/ChallengeDurationView.swift
@@ -0,0 +1,18 @@
+//
+//  ChallengeDurationView.swift
+//  Challenge Self
+//
+//  Created by Kirills Galenko on 28/08/2024.
+//
+
+import SwiftUI
+
+struct ChallengeDurationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChallengeDurationView()
+}
